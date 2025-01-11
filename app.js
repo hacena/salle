0bc9428a8f26bb18caa3a31fbdd522ec3becd10e
hacena <ha2502ha@gmail.com>
2025-01-11 21:08:57 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+<form action="/signup" method="POST">
+    <input type="ha2502ha@" name="email" placeholder="البريد الإلكتروني" required>
+    <input type="HA4141ha@" name="password" placeholder="كلمة السر" required>
+    <button type="submit">تسجيل</button>
+</form>
