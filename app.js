3b5c1cc194e5cda34861f95c26de0ea27ea4257f
hacena <ha2502ha@gmail.com>
2025-01-12 00:07:41 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser'); // إضافة body-parser
 const app = express();
 const PORT = 3000;
 
 // إعداد Express لتقديم الملفات من مجلد 'public'
 app.use(express.static('public'));
-
-// استخدام body-parser لتحليل البيانات الواردة من نموذج التسجيل
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 // إرسال الصفحة الرئيسية أو أي صفحة أخرى
 app.get('/', (req, res) => {
@@ -19,7 +14,7 @@ app.get('/', (req, res) => {
 app.post('/signup', (req, res) => {
     const { email, password } = req.body;
     // معالجة بيانات التسجيل
-    res.send(`تم التسجيل بنجاح. البريد: ${email} وكلمة المرور: ${password}`);
+    res.send(`تم التسجيل بنجاح. البريد: ${email}`);
 });
 
 // بدء الخادم
